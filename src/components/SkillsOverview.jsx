ec016fa5e45dcbf5d3cc9bfd36cf1d329128e4d9
Liam Appleton <liamappleton97@outlook.com>
2025-05-15 15:57:37 +0100

    creating SkillsOverview

---
+++ src/components/SkillsOverview.jsx
@@ -0,0 +1,20 @@
+import { SkillCard } from './SkillCard';
+import { skills } from '../data/skills';
+
+export const SkillsOverview = () => {
+  return (
+    <>
+      <h1>Skills Overview</h1>
+      <SkillCard
+        title={skills.languages.title}
+        skills={skills.languages.skills}
+      />
+      <SkillCard
+        title={skills.frontEnd.title}
+        skills={skills.frontEnd.skills}
+      />
+      <SkillCard title={skills.backEnd.title} skills={skills.backEnd.skills} />
+      <SkillCard title={skills.general.title} skills={skills.general.skills} />
+    </>
+  );
+};
