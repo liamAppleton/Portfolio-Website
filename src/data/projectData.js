5ae80f723fe91a8661ee3fcd759c59d54bce472d
Liam Appleton <liamappleton97@outlook.com>
2025-05-15 16:37:12 +0100

    creating projectData

---
+++ src/data/projectData.js
@@ -0,0 +1,14 @@
+import ncnewsfe from '../assets/NCnewsfrontend.png';
+
+export const projects = {
+  ncNewsFe: {
+    title: 'NC News (front end)',
+    description:
+      'Built with React and styled with Bootstrap, NC News is a Reddit-style web application that allows users to vote on articles and comments, post comments, filter and sort content by various criteria, and even toggle between light and dark modes — all powered by a custom back-end API.',
+    links: {
+      github: 'https://github.com/liamAppleton/nc-news',
+      hosted: 'https://nc-hub.netlify.app/',
+    },
+    image: ncnewsfe,
+  },
+};
