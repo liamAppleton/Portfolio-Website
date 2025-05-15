94c86698aee4fc5130d071a5eafd17b704500ae1
Liam Appleton <liamappleton97@outlook.com>
2025-05-15 22:17:52 +0100

    changing styling using bootstrap row and col

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,32 +1,43 @@
-import Card from 'react-bootstrap/Card';
+import { Card, Row, Col } from 'react-bootstrap';
 
 export const ProjectCard = ({ projectData }) => {
   const { title, description, links, image } = projectData;
   return (
-    <Card style={{ width: '15rem' }}>
-      <Card.Img variant="top" src={image} />
-      <Card.Body>
-        <Card.Title>{title}</Card.Title>
-        <Card.Text>{description}</Card.Text>
-      </Card.Body>
-      <Card.Body>
-        {links.hosted && (
-          <Card.Link
-            href={links.hosted}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Hosted version
-          </Card.Link>
-        )}
-        <Card.Link
-          href={links.github}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          GitHub repo
-        </Card.Link>
-      </Card.Body>
+    <Card style={{ maxWidth: '100%' }} className="mx-auto">
+      <Row className="g-0">
+        <Col xs={12} md={4} className="d-flex align-items-center">
+          <Card.Img
+            src={image}
+            style={{ width: '100%', height: 'auto', objectFit: 'cover' }}
+          />
+        </Col>
+        <Col xs={12} md={8}>
+          <Card.Body>
+            <Card.Title>{title}</Card.Title>
+            <Card.Text style={{ fontSize: 'clamp(1rem, 1.5vw, 1.2rem)' }}>
+              {description}
+            </Card.Text>
+            <div>
+              {links.hosted && (
+                <Card.Link
+                  href={links.hosted}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Hosted version
+                </Card.Link>
+              )}
+              <Card.Link
+                href={links.github}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                GitHub repo
+              </Card.Link>
+            </div>
+          </Card.Body>
+        </Col>
+      </Row>
     </Card>
   );
 };
