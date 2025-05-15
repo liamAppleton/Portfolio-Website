710679d359cb4ccf1e533703b260e072dfc3f5bb
Liam Appleton <liamappleton97@outlook.com>
2025-05-15 16:36:50 +0100

    creating ProjectCard

---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,32 @@
+import Card from 'react-bootstrap/Card';
+
+export const ProjectCard = ({ projectData }) => {
+  const { title, description, links, image } = projectData;
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={image} />
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text>{description}</Card.Text>
+      </Card.Body>
+      <Card.Body>
+        {links.hosted && (
+          <Card.Link
+            href={links.hosted}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Hosted version
+          </Card.Link>
+        )}
+        <Card.Link
+          href={links.github}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub repo
+        </Card.Link>
+      </Card.Body>
+    </Card>
+  );
+};
