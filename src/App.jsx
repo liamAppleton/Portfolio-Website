d828897ab024f5b1c8f19bda5c2b78008bad8a56
Liam Appleton <liamappleton97@outlook.com>
2025-05-15 16:38:38 +0100

    rendering ProjectsDisplay on App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import './App.css';
 import { SkillsOverview } from './components/SkillsOverview';
+import { ProjectsDisplay } from './components/ProjectsDisplay';
 
 function App() {
-  return <SkillsOverview />;
+  return (
+    <>
+      <SkillsOverview />
+      <ProjectsDisplay />
+    </>
+  );
 }
 
 export default App;
