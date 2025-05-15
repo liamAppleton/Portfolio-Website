155cc29ac5e2a62cd6accb0747498d890c92ef8d
Liam Appleton <liamappleton97@outlook.com>
2025-05-15 15:46:39 +0100

    adding skills data

---
+++ src/data/skills.js
@@ -0,0 +1,34 @@
+export const skills = {
+  languages: {
+    title: 'Languages',
+    skills: ['JavaScript', 'C#'],
+  },
+  frontEnd: {
+    title: 'Front end',
+    skills: ['HTML', 'CSS', 'React', 'Bootstrap', 'Chrome Developer Tools'],
+  },
+  backEnd: {
+    title: 'Back end',
+    skills: ['Node.js', 'Express.js', 'PostgreSQL'],
+  },
+  testing: {
+    title: 'Testing',
+    skills: [
+      'Test Driven Development',
+      'Jest',
+      'Supertest',
+      'Postman',
+      'Insomnia',
+    ],
+  },
+  general: {
+    title: 'General',
+    skills: [
+      'Version Control - Git/GitHub',
+      'Object Oriented Programming',
+      'Paired Programming | Mobbing',
+      'Spiking',
+      'Technical Communication',
+    ],
+  },
+};
