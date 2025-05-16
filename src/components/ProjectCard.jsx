90db9d71a5fa1055b89ea22b5c6a8be7e71d5f01
Liam Appleton <liamappleton97@outlook.com>
2025-05-16 16:11:25 +0100

    adjusting styling so icons are at the bottom

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,8 +1,9 @@
 import { Card, Row, Col } from 'react-bootstrap';
 import { FaGithub } from 'react-icons/fa';
+import { IconBar } from './IconBar';
 
 export const ProjectCard = ({ projectData }) => {
-  const { title, description, links, image } = projectData;
+  const { title, description, links, image, icons } = projectData;
   return (
     <Card style={{ maxWidth: '80%' }} className="mx-auto p-0 project-card">
       <Row className="g-0">
@@ -16,32 +17,35 @@ export const ProjectCard = ({ projectData }) => {
             }}
           />
         </Col>
-        <Col md={12} lg={8}>
-          <Card.Body>
+        <Col md={12} lg={8} className="d-flex flex-column">
+          <Card.Body className="d-flex flex-column">
             <Card.Title>{title}</Card.Title>
             <Card.Text style={{ fontSize: 'clamp(1rem, 1.5vw, 1.2rem)' }}>
               {description}
             </Card.Text>
-            <div className="d-flex flex-column align-items-start gap-2">
-              <Card.Link
-                href={links.github}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="p-0 m-0"
-              >
-                <FaGithub size={25} />
-              </Card.Link>
 
-              {links.hosted && (
+            <div className="mt-auto d-flex justify-content-between align-items-center">
+              <div className="d-flex gap-2">
                 <Card.Link
-                  href={links.hosted}
+                  href={links.github}
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="p-0 m-0"
                 >
-                  Hosted version
+                  <FaGithub size={25} />
                 </Card.Link>
-              )}
+
+                {links.hosted && (
+                  <Card.Link
+                    href={links.hosted.link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {<links.hosted.icon size={25} />}
+                  </Card.Link>
+                )}
+              </div>
+
+              <IconBar icons={icons} />
             </div>
           </Card.Body>
         </Col>
