a2c9aa418b3dab921c467fc085d57f7d9add6891
Liam Appleton <liamappleton97@outlook.com>
2025-05-16 19:38:50 +0100

    importing icons into skillsData

--- src/data/skillData.js
+++ src/data/skillData.js
@@ -1,15 +1,23 @@
+import { RiComputerLine } from 'react-icons/ri';
+import { FaServer, FaCode } from 'react-icons/fa6';
+import { GrTest } from 'react-icons/gr';
+import { GiSkills } from 'react-icons/gi';
+
 export const skills = {
   languages: {
     title: 'Languages',
     skills: ['JavaScript', 'C#'],
+    Icon: FaCode,
   },
   frontEnd: {
     title: 'Front end',
     skills: ['HTML', 'CSS', 'React', 'Bootstrap', 'Chrome Developer Tools'],
+    Icon: RiComputerLine,
   },
   backEnd: {
     title: 'Back end',
     skills: ['Node.js', 'Express.js', 'PostgreSQL'],
+    Icon: FaServer,
   },
   testing: {
     title: 'Testing',
@@ -20,6 +28,7 @@ export const skills = {
       'Postman',
       'Insomnia',
     ],
+    Icon: GrTest,
   },
   general: {
     title: 'General',
@@ -30,5 +39,6 @@ export const skills = {
       'Spiking',
       'Technical Communication',
     ],
+    Icon: GiSkills,
   },
 };
