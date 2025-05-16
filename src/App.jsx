62a0b7a10f475f5869c1336a8db5b43bd0be54a5
Liam Appleton <liamappleton97@outlook.com>
2025-05-16 22:25:55 +0100

    rendering PersonalBio in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import { SkillsOverview } from './components/SkillsOverview';
 import { ProjectsDisplay } from './components/ProjectsDisplay';
+import { PersonalBio } from './components/PersonalBio';
 
 function App() {
   return (
     <>
+      <PersonalBio />
       <SkillsOverview />
       <ProjectsDisplay />
     </>
