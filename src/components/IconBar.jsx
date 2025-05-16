48a7262a5624e7ffb0242d6688a34d63a2631aba
Liam Appleton <liamappleton97@outlook.com>
2025-05-16 16:11:59 +0100

    creating IconBar component

---
+++ src/components/IconBar.jsx
@@ -0,0 +1,9 @@
+export const IconBar = ({ icons }) => {
+  return (
+    <div className="d-flex gap-2">
+      {icons.map((Icon, i) => (
+        <Icon key={i} size={25} />
+      ))}
+    </div>
+  );
+};
